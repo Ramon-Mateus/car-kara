3d2c5107edc957c8d607b8668dd030c8d7cdacbb
Ramon-Mateus <ramonmateus00@gmail.com>
2025-03-22 01:14:41 -0300

    ajustes

--- script.js
+++
@@ -1,5 +0,0 @@
-// Exemplo de JavaScript para interatividade
-document.getElementById('form-contato').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Obrigado por entrar em contato! Responderemos em breve.');
-});
