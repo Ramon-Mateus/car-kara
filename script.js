d0967cd62b5d2f8b099b3bc6a60952c9af6f23cb
Ramon-Mateus <ramonmateus00@gmail.com>
2025-03-22 01:07:12 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Exemplo de JavaScript para interatividade
+document.getElementById('form-contato').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Obrigado por entrar em contato! Responderemos em breve.');
+});
